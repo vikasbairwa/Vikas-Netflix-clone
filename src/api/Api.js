47452575fbd4e542bcbcefb8e9379e4b426e0301
Vikas <bairwavikas5@gmail.com>
2021-10-24 13:02:35 +0530

    backdrop change

--- src/api/Api.js
+++ src/api/Api.js
@@ -4,7 +4,7 @@ const api_key = process.env.REACT_APP_API ;
 const BaseUrl = "https://api.themoviedb.org/3";
 
 const Api=(searchpath, params)=>{
-    console.log(api_key)
+    // console.log(api_key)
     const searchApi = qs.stringify({api_key, ...params});
     
      const initialUrl = `${BaseUrl}${searchpath}`;
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,29 +4,34 @@ import './css/Banner.css'
 
 function Banner() {
     const ApiKey = process.env.REACT_APP_API;
+    const page = Math.floor(Math.random() * 500 +1);
     const BaseUrl = "https://api.themoviedb.org/3/discover/movie?api_key="
     const [bannerMovie, setBannerMovie] = useState({});
 
     useEffect(() => {
         const fetchMovie = async () => {
-            const url = `${BaseUrl}${ApiKey}`
+            try {
+                const url = `${BaseUrl}${ApiKey}&page=${page}`
             let data = await fetch(url);
             let parsedData = await data.json();
 
             setBannerMovie(
                 parsedData.results[
-                Math.floor(Math.random() * parsedData.results.length + 1)
+                Math.floor(Math.random() * parsedData.results.length)
                 ]
             );
+            } catch (error) {
+                console.log(error.message);
+            }
         }
         fetchMovie();
         // eslint-disable-next-line
     }, [])
 
-
+    const backdrop = bannerMovie.backdrop_path? bannerMovie.backdrop_path:'/VuukZLgaCrho2Ar8Scl9HtV3yD.jpg';
     return (
 
-        <div className="banner" style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w1280${!bannerMovie.backdrop_path ?'/VuukZLgaCrho2Ar8Scl9HtV3yD.jpg': bannerMovie.backdrop_path})` }} >
+        <div className="banner" style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w1280${backdrop})` }} >
             <div className="content">
 
                 <h1 className="title">{bannerMovie.title ? bannerMovie.title : bannerMovie.original_title}</h1>
